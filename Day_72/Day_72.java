bf7fce0ab7d505c6078ef1131be6f4f95d300482
DIVyanshu1862 <144006346+DIVyanshu1862@users.noreply.github.com>
2024-01-24 22:04:00 +0530

    Add files via upload

---
+++ Day_72/Day_72.java
@@ -0,0 +1,27 @@
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        ArrayList<Integer> ans=new ArrayList<>();
+        Queue<Integer> q=new LinkedList<>();
+        boolean arr[]=new boolean[adj.size()];
+        q.add(0);
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+
+            if (!arr[curr]) {
+                ans.add(curr);
+                arr[curr] = true;
+
+                for (int i = 0; i < adj.get(curr).size(); i++) {
+                    int e = adj.get(curr).get(i);
+                    if (!arr[e]) {
+                        q.add(e);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+    
+}
