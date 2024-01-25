4f9d1e5952d93ce1f2375f84c68948ec126e1916
DIVyanshu1862 <144006346+DIVyanshu1862@users.noreply.github.com>
2024-01-25 21:50:42 +0530

    Add files via upload

---
+++ Day_73/Day_73.java
@@ -0,0 +1,21 @@
+class Solution {
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
+        ArrayList<Integer> ans = new ArrayList<>();
+        boolean[] vis = new boolean[V];
+    
+        dfs(adj, vis, ans, 0);
+
+        return ans; 
+    }
+    
+    void dfs(ArrayList<ArrayList<Integer>> adj, boolean[] vis, ArrayList<Integer> ans, int curr){
+        ans.add(curr);
+        vis[curr] = true;
+        
+        for(Integer temp: adj.get(curr)){
+           if(vis[temp] != true)
+             dfs(adj, vis, ans, temp);
+        }
+    }
+}
